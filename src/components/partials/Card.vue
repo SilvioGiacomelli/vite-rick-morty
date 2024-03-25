0480c2b5095ba9da506b2631534d12a8e32c2471
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-25 17:42:22 +0100

    [Finito]

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -16,8 +16,9 @@
       :src="image" class="card-img-top" 
       :alt="name">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">{{ name }}</h5>
+        <p class="card-text">{{status}}</p>
+        <p class="card-text">{{ species }}</p>
       </div>
     </div>
   </div>
